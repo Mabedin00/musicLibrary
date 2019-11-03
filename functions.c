cb2cceb49f6e9ba028764b7d5cce70d3a16d2d28
Brian <bmoses00@stuy.edu>
2019-11-03 12:31:19 -0500

    Completed functions

--- functions.c
+++ functions.c
@@ -3,6 +3,7 @@
 # include "headers.h"
 # include <string.h>
 # include <time.h>
+# include <ctype.h>
 
 struct node * create_node(char newName[100], char newArtist[100] ) {
   struct node * head = calloc(208, 1);
@@ -94,6 +95,7 @@ struct node * return_first_of_artist(struct node * node, char newArtist[100]) {
     if (strcmp(node->artist, newArtist) == 0)
       return node;
 }
+
 int length_of_list(struct node * node){
   int length;
   for (length = 0; node != NULL; node = node->next, length += 1) {}
@@ -104,20 +106,89 @@ struct node * return_song_by_number(struct node * node, int number){
   int counter;
   for (counter = 0; counter != number; node = node->next, counter++){}
   return node;
-
 }
 
-
-void shuffle(struct node * node) {
-  struct node * copyList = node;
-  int length = length_of_list(copyList);
+struct node * return_random(struct node * node) {
   srand(time(NULL));
-  while(length != 0){
-    int desiredNode = rand() % length;
-    struct node * temp = return_song_by_number(copyList, desiredNode);
-    printf("Name: %s, Artist: %s\n", temp->name, temp->artist);
-    copyList = remove_node(copyList, temp->name, temp->artist);
-    length--;
+  int length = length_of_list(node);
+  int desiredNode = rand() % length;
+  return return_song_by_number(node, desiredNode);
+}
+
+// -------------------------------------------------------------
+
+void shuffle(struct node ** library) {
+  srand(time(NULL));
+  int i, songs;
+  float chance;
+  for (i = 0, songs = 0; i < 27; i++)
+      songs += length_of_list(library[i]);
+
+  for (i = 0; i < 27; i++) {
+    struct node * temp = library[i];
+    for ( ; temp != NULL; temp = temp->next) {
+      if (rand() % songs > songs / 2) {
+        printf("%s: %s\n", temp->name, temp->artist);
+      }
+    }
   }
-  free_list(copyList);
+}
+
+int position_in_alpha(char artist[100]) {
+  char first = artist[0];
+  if (first > 96 && first < 123) return first - 97;
+  if (first > 64 && first < 91)  return first - 65;
+  else return 27;
+  }
+
+void add_song(struct node ** library, char newName[100], char newArtist[100]) {
+  int pos = position_in_alpha(newArtist);
+  if (library[pos] == NULL)
+    library[pos] = create_node(newName, newArtist);
+  else
+    library[pos] = insert_in_order(library[pos], newName, newArtist);
+}
+
+struct node * song_from_library(struct node ** library, char newName[100], char newArtist[100]) {
+  return return_song(library[position_in_alpha(newArtist)], newName, newArtist);
+}
+
+void print_all_from_letter(struct node ** library, char * letter) {
+  print_list(library[position_in_alpha(letter)]);
+}
+
+void print_library(struct node ** library) {
+  int i;
+  for (i = 0; i < 27; i += 1)
+    if (library[i] != NULL)
+      print_list(library[i]);
+}
+
+void free_library(struct node ** library) {
+  int i;
+  for (i = 0; i < 27; i += 1)
+      library[i] = free_list(library[i]);
+}
+
+struct node * delete_song(struct node ** library, char newName[100], char newArtist[100]) {
+  return remove_node(library[position_in_alpha(newArtist)], newName, newArtist);
+}
+
+int find_artist(struct node ** library, char newArtist[100]) {
+  struct node * temp = library[position_in_alpha(newArtist)];
+  for ( ; temp != NULL; temp = temp->next) {
+    if (strcmp(temp->artist, newArtist) == 0) {
+      print_list(temp);
+      return 0;
+    }
+  }
+  printf("Artist not found\n");
+  return 1;
+}
+
+void print_artist(struct node ** library, char newArtist[100]) {
+  struct node * temp = library[position_in_alpha(newArtist)];
+  for ( ; temp != NULL; temp = temp->next)
+    if (strcmp(temp->artist, newArtist) == 0)
+      printf("%s: %s\n", temp->name, temp->artist);
 }
--- main.c
+++ main.c
@@ -5,42 +5,40 @@
 
 int main() {
 
-  struct node * head = NULL;
-
+  // struct node * head = NULL;
+  //
   char name[100] = "Thriller";
   char artist[100] = "Michael Jackson";
-  printf("Insert Thriller by Michael Jackson\n");
-  head = insert_in_order(head, name, artist);
+  // printf("Insert Thriller by Michael Jackson\n");
+  // head = insert_in_order(head, name, artist);
+  //
+  // strcpy(name, "Rocket Man");
+  // strcpy(artist, "Elton John");
+  // printf("Insert Rocket Man by Elton John\n");
+  // head = insert_in_order(head, name, artist);
+  //
+  // strcpy(name, "Tiny Dancer");
+  // strcpy(artist, "Elton John");
+  // printf("Insert Tiny Dancer by Elton John\n");
+  // head = insert_in_order(head, name, artist);
+  //
+  // strcpy(name, "Time");
+  // strcpy(artist, "Pink Floyd");
+  // printf("Insert Time by Pink Floyd\n");
+  // head = insert_in_order(head, name, artist);
+  //
+  // strcpy(name, "Stressed Out");
+  // strcpy(artist, "21 Pilots");
+  // printf("Insert Stressed Out by 21 Pilots\n");
+  // head = insert_in_order(head, name, artist);
+  //
+  // strcpy(name, "Numb");
+  // strcpy(artist, "Linkin Park");
+  // printf("Insert Numb by Linkin Park\n");
+  // head = insert_in_order(head, name, artist);
 
-  strcpy(name, "Rocket Man");
-  strcpy(artist, "Elton John");
-  printf("Insert Rocket Man by Elton John\n");
-  head = insert_in_order(head, name, artist);
-
-  strcpy(name, "Tiny Dancer");
-  strcpy(artist, "Elton John");
-  printf("Insert Tiny Dancer by Elton John\n");
-  head = insert_in_order(head, name, artist);
-
-  strcpy(name, "Time");
-  strcpy(artist, "Pink Floyd");
-  printf("Insert Time by Pink Floyd\n");
-  head = insert_in_order(head, name, artist);
-
-  strcpy(name, "Stressed Out");
-  strcpy(artist, "21 Pilots");
-  printf("Insert Stressed Out by 21 Pilots\n");
-  head = insert_in_order(head, name, artist);
-
-  strcpy(name, "Numb");
-  strcpy(artist, "Linkin Park");
-  printf("Insert Numb by Linkin Park\n");
-  head = insert_in_order(head, name, artist);
-
-  printf("\n");
-  print_list(head);
-
-  // // print_list(head);
+  // printf("\n");
+  // print_list(head);
   //
   // strcpy(name, "Stressed Out");
   // strcpy(artist, "21 Pilots");
@@ -66,11 +64,58 @@ int main() {
   // printf("Printing first song by Michael Jackson\n");
   //
   // print_list(return_first_of_artist(head, artist));
+  //
+  // printf("Printing random node and following nodes\n");
+  // print_list(return_random(head));
+  //
+  // printf("Freeing list\n");
+  // head = free_list(head);
 
-  printf("Printing random node and following nodes\n");
-  shuffle(head);
+  printf("Testing Lib Functions\n\n");
+  struct node * library[216];
 
-  printf("Freeing list\n");
-  head = free_list(head);
+  int i;
+  for (i = 0; i < 27; i++) {
+    library[i] = 0;
+  }
+  printf("%p\n", library);
+  add_song(library, name, artist);
 
+  printf("%s\n", library[12]->artist);
+
+  char name2[100] = "Beat It";
+  char artist2[100] = "Michael Jackson";
+
+  add_song(library, name2, artist2);
+
+  print_list(library[12]);
+
+  print_list(song_from_library(library, name, artist));
+
+  print_all_from_letter(library, "m");
+
+  print_library(library);
+
+  // printf("Freeing library\n");
+  // free_library(library);
+
+  // print_library(library);
+
+  // delete_song(library, name, artist);
+
+  print_library(library);
+
+  char name3[100] = "Elton John";
+
+  find_artist(library, name3);
+
+  char artist3[100] = "Elton John";
+  char name4[100] = "Tiny Dancer";
+
+  add_song(library, name4, artist3);
+
+  printf("printing artist\n");
+  print_artist(library, artist2);
+
+  shuffle(library);
 }
